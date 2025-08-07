45e8e88d78de2ad7918a406cffeda3a3272cfdeb
Lezek123 <l.wiesner@subsquid.io>
2025-08-07 14:51:20 +0200

    Solana swaps: Add missing config

--- pipes/solana/swaps/config.ts
+++ pipes/solana/swaps/config.ts
@@ -4,13 +4,16 @@ import { getCommonConfig } from '../config';
 export function getConfig() {
   const commonConfig = getCommonConfig();
   const dbPath = process.env.DB_PATH;
+  const cacheDumpPath = process.env.CACHE_DUMP_PATH;
   const onlyMeta = ['t', 'T', 'true', '1'].includes(process.env.ONLY_META || '');
 
   assert(dbPath, 'DB_PATH param must be specified');
+  assert(cacheDumpPath, 'CACHE_DUMP_PATH param must be specified');
 
   return {
     ...commonConfig,
     dbPath,
+    cacheDumpPath,
     onlyMeta,
   };
 }
